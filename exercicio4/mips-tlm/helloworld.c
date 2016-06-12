4bff72df3e509a3af738202f1552ebaec7827b5a
Grupo 06 - João Guilherme Daros Fidelis <joaogfidelis@gmail.com>
2016-06-12 10:53:36 -0300

    finished application

--- exercicio4/mips-tlm/helloworld.c
+++ exercicio4/mips-tlm/helloworld.c
@@ -1,64 +1,71 @@
 #include <stdio.h>
-
+#include <string.h>
 #define ADDRESS 0x6410000;
-#define LOCK_SUM 0x6420000;
 
 volatile int procCounter = 0;
 
+#define THREAD_NUM 2
+#define SIZE 26
 volatile int *lock = (volatile int *) ADDRESS;
 
-//volatile int *lockSum = (volatile int *) LOCK_SUM;
-
-volatile int a = 0, b = 0;
-
+int hista[SIZE];
+int histb[SIZE];
+volatile int done=0;
 void AcquireLock() {
-//	printf("LOCK %d\n", *lock);
 	while (*lock);
-//	printf("PASSEI\n");
 }
 
 void ReleaseLock() {
-//	printf("RELEASING LOCK\n");
 	*lock = 0;
-//	printf("TESTE\n");
+}
+
+void g() {
+	int procNumber;
+	AcquireLock();
+	procNumber = procCounter++;
+	ReleaseLock();
+	//input
+	char *str = "noqncjueyuirytcnbcnbvbbvaasoaospasskaposkderyuwetuygbcbchjbvhvgegwgccyvwytynbcvpqwiufyqamxbchkz\0";
+	//input size
+	int n = strlen(str);
+	int i;
+	//first core will process the first half of the string
+	if (procNumber == 0) {
+		for (i=n*procNumber/THREAD_NUM; i < n*(procNumber+1)/THREAD_NUM; i++) {
+			int num = str[i] - 'a';
+			hista[num]++;
+		}
+	AcquireLock();
+	done++;
+	ReleaseLock();
+	}
+
+	//second core will process the second half of the string
+	else if (procNumber == 1) {
+		for (i=n*procNumber/THREAD_NUM; i < n; i++) {
+			int num = str[i] - 'a';
+			histb[num]++;
+		}
+		AcquireLock();
+		done++;
+		ReleaseLock();
+	}
+	//after both are done, the first core will merge the two partial histograms and display the output
+	if (procNumber == 0) {
+		int hist[SIZE];
+		while(done < THREAD_NUM);
+		for(i = 0; i < SIZE; i++) hist[i] = hista[i]+histb[i];
+		for (i = 0; i < SIZE; i++) {
+			printf("%d ", hist[i]);
+		}
+		printf("\ndone!\n");
+	}
 
 }
 
 int main() {
-	int procNumber;
-//	printf("getting lock\n");
-	AcquireLock();
-//	printf("got lock\n");
-	procNumber = procCounter;
-	procCounter++;
-	//printf("Core %d releasing lock...\n", procNumber+1);
-	ReleaseLock();
-	int N = 200;
-	int vet[N];
-	int i;
-	for (i = 0; i < N; i++) vet[i] = i;
 
-	if (procNumber == 0) {
-		for (i = 0; i < N/2; i++) {
-			a += vet[i];
-		}
-	}
+	g();
 
-
-	else if (procNumber == 1) {
-		AcquireLock();
-		for (i = N/2; i < N; i++){
-			b += vet[i];
-		}
-		ReleaseLock();
-	}
-	int c;
-	if (procNumber == 0) {
-		AcquireLock();
-		c = a + b;
-		ReleaseLock();
-	}
-	if (procNumber == 0)
-		printf("SOMA %d\n", c);
 	return 0;
 }
